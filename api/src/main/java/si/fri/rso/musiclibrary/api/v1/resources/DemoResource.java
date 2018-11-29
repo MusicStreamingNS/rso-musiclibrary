983b76de4d8d76b5877d06a4172ba6645566220d
Nemanja <soldo.nemanja@gmail.com>
2018-11-29 14:43:51 +0100

    test1

--- api/src/main/java/si/fri/rso/musiclibrary/api/v1/resources/DemoResource.java
+++ api/src/main/java/si/fri/rso/musiclibrary/api/v1/resources/DemoResource.java
@@ -63,10 +63,10 @@ public class DemoResource {
         JsonObject json = Json.createObjectBuilder()
                 .add("clani", Json.createArrayBuilder().add("ns8143"))
                 .add("opis_projekta", "My project implements application for music streaming.")
-                .add("mikrostoritve", Json.createArrayBuilder().add("http://159.122.187.177:32399/v1/customers"))
-                .add("github", Json.createArrayBuilder().add("https://github.com/jmezna/rso-customers"))
-                .add("travis", Json.createArrayBuilder().add("https://travis-ci.org/jmezna/rso-customers"))
-                .add("dockerhub", Json.createArrayBuilder().add("https://hub.docker.com/r/jmezna/rso-customers"))
+                .add("mikrostoritve", Json.createArrayBuilder().add("http://159.122.187.177:30999/v1/songs"))
+                .add("github", Json.createArrayBuilder().add("https://github.com/MusicStreamingNS/rso-musiclibrary"))
+                .add("travis", Json.createArrayBuilder().add("https://travis-ci.org/MusicStreamingNS/rso-musiclibrary"))
+                .add("dockerhub", Json.createArrayBuilder().add("https://hub.docker.com/r/nemanjas23/rso-musiclibrary/"))
                 .build();
 
 
